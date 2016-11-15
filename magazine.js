731767711a5c1e5e6c7bb48bd107fc62b956e4ef
Pierce McBride <piercermcbride@gmail.com>
2016-11-15 13:26:10 -0500

    Github pages test

--- magazine.js
+++ magazine.js
@@ -4,8 +4,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
-			this.classList.toggle("active");
-			this.nextElementSibling.classList.toggle("show");
-			console.log("this");
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
     }
 }
