2f35c94ef9676499f9bef67f583250ebcc0c70bf
Pierce McBride <piercermcbride@gmail.com>
2016-12-06 12:12:38 -0500

    Altered the Local Storage Code
    Now each magazine has a unique local storage variable

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,32 +1,35 @@
 function setStorage(chosenGame, cvalue){
-    localStorage.setItem("choice", chosenGame);
-    var result = localStorage.getItem("choice");
+    localStorage.setItem(chosenGame, cvalue);
+    //var result = localStorage.getItem("choice");
     checkCookieStatus(cvalue);
 }
 
-function getCookie() {
-    var result = localStorage.getItem("choice");
-    console.log(result);
+function getCookie(localstoragevar) {
+    var result = localStorage.getItem(localstoragevar);
     return result;
 }
 
-function checkCookie() {
-    var choice = getCookie();
-    console.log(choice);
-    if (choice === "chosenGame1") {
-        checkCookieStatus(1);
-    }
-    else if (choice === "chosenGame2") {
-        checkCookieStatus(2);
-    }
-    else if (choice === "chosenGame3") {
-        checkCookieStatus(3);
-    }
-    else{
-        return;
-    }
+function checkCookie(localstoragevar) {
+	var choice = getCookie(localstoragevar);
+	console.log(choice);
+	/*
+	if (Number(choice) === 1) {
+		checkCookieStatus(1);
+	}
+	else if (Number(choice) === 2) {
+			checkCookieStatus(2);
+	}
+	else if (Number(choice) === 3) {
+			checkCookieStatus(3);
+	}*/
+	if (Number(choice) != null) {
+		checkCookieStatus(Number(choice));
+	}
+	else{
+		return;
+	}
 }
-
+/*
 function checkCookieStatus(cvalue){
     if(cvalue===1){
     document.getElementById("game").className = "btn-flat"
@@ -50,3 +53,4 @@ function checkCookieStatus(cvalue){
     document.getElementById("decision3").innerHTML = "You chose this game!";
     }
 }
+*/
