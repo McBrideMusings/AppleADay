855e2fdf0650e43bc098b45e7d796946e280200a
kamehouseorbust <haydendrussell@gmail.com>
2018-03-29 22:26:24 -0400

    Add files via upload
    Updated final JS files

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,6 +1,5 @@
 function setStorage(localstoragevar, cvalue){
     localStorage.setItem(localstoragevar, cvalue);
-    //var result = localStorage.getItem("choice");
     checkCookieStatus(localstoragevar);
     console.log(cvalue);
 }
@@ -20,28 +19,3 @@ function checkCookie(localstoragevar) {
 		return;
 	}
 }
-/*
-function checkCookieStatus(cvalue){
-    if(cvalue===1){
-    document.getElementById("game").className = "btn-flat"
-    document.getElementById("game").href="https://www.google.com/";
-    document.getElementById("decision1").innerHTML = "You chose this game!";
-    document.getElementById("decision2").style.visibility = "hidden";
-    document.getElementById("decision3").style.visibility = "hidden";
-    }
-    if(cvalue===2){
-    document.getElementById("game").className = "btn-flat"
-    document.getElementById("game").href="https://www.google.com/";
-    document.getElementById("decision1").style.visibility = "hidden";
-    document.getElementById("decision2").innerHTML = "You chose this game!";
-    document.getElementById("decision3").style.visibility = "hidden";
-    }
-    if(cvalue===3){
-    document.getElementById("game").className = "btn-flat"
-    document.getElementById("game").href="https://www.google.com/";
-    document.getElementById("decision1").style.visibility = "hidden";
-    document.getElementById("decision2").style.visibility = "hidden";
-    document.getElementById("decision3").innerHTML = "You chose this game!";
-    }
-}
-*/
